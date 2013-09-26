83ea2f5a8af7e41a53af9d76732f9ecdd4c6f183
mrgamer <mister.gamer@gmail.com>
2013-09-26 17:47:15 +0200

    Comment functionality added closes #2

--- tasks/pgutils.js
+++ tasks/pgutils.js
@@ -1,5 +1,9 @@
+// Internal Libs
+var path = require('path');
+// External Deps
 var pg = require('pg'),
-    cp = require('child_process');
+    cp = require('child_process'),
+    S = require('string');
 
 module.exports = function (grunt) {
   var config = grunt.config.get('pgutils'),
@@ -11,23 +15,47 @@ module.exports = function (grunt) {
       pgClient = null,
       // Success count
       success = 0,
-      query;
+      // query
+      listSPsql,
+      listSP;
 
-    config.dest = config.dest || 'spsql/';
     var serverConfig = (this.args.length)? config.connections[parseInt(this.args[0], 10)] : config.connections[0];
+    config.dest = config.dest || 'spsql/';
+    listSPsql = "select " +
+      "sp.proname as fname, " +
+      "n.nspname as nspace, " +
+      "sp.pronargs as fargs, " +
+      "regexp_replace(CAST (sp.proargnames as text), '[\{\}]', '', 'g') as fargv, " +
+      "pg_get_function_identity_arguments(sp.oid) as fargdesc, " +
+      "pg_get_functiondef(sp.oid) as fdef, " +
+      "ds.description as fdesc from " +
+      "pg_proc as sp LEFT OUTER JOIN pg_description ds ON ds.objoid = sp.oid " +
+        "INNER JOIN pg_namespace n ON sp.pronamespace = n.oid " +
+        "WHERE sp.proname ~ '" + config.spRegex + "'";
 
     pgClient = new pg.Client(serverConfig);
     // Actually connecting to postgreSQL
     pgClient.connect();
 
-    query = pgClient.query("select pg_get_functiondef(sp.oid) as functiondef, sp.proname as proname, sp.pronargs as pronargs from (select oid, proname, pronargs from pg_proc where proname ~ '" + config.spRegex + "') as sp");
-    query.on('row', function (row, result) {
-      grunt.file.write(config.dest + '/' + row.proname + '_' + row.pronargs + '.sql', row.functiondef);
+    listSP = pgClient.query(listSPsql);
+    listSP.on('row', function (sp, result) {
+      var filename,
+          filecontent,
+          spcomment;
+
+      filename = S('{{fname}} - {{fargs}}args ({{fargv}})' + '.sql').template(sp);
+      filecontent = S("{{fdef}};\n").template(sp);
+      if (sp.fdesc) {
+        filecontent += S("COMMENT ON FUNCTION {{nspace}}.{{fname}}({{fargdesc}})\n" +
+          "IS '{{fdesc}}'").template(sp);
+      }
+
+      grunt.file.write(config.dest + filename, filecontent);
       success++;
     });
 
-    query.on('error', grunt.fail.fatal);
-    query.on('end', function () {
+    listSP.on('error', grunt.fail.fatal);
+    listSP.on('end', function () {
       grunt.log.ok('Correctly backupped ' + success + ' stored procedures.');
       done();
     });
@@ -45,8 +73,8 @@ module.exports = function (grunt) {
       success = 0,
       query;
 
+    var serverConfig = (this.args.length) ? config.connections[parseInt(this.args[0], 10)] : config.connections[0];
     config.src = config.src || 'spsql/*.js';
-    var serverConfig = (this.args.length)? config.connections[parseInt(this.args[0], 10)] : config.connections[0];
 
     pgClient = new pg.Client(serverConfig);
 
