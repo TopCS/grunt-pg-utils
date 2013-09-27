8cec6f450947d00c75ecca5cfc190d3ec1010e0a
mrgamer <mister.gamer@gmail.com>
2013-09-27 19:03:39 +0200

    'dump' Task now make use of utilities

--- tasks/dump.js
+++ tasks/dump.js
@@ -5,23 +5,14 @@ var path = require('path');
 var pg = require('pg'),
     cp = require('child_process'),
     S = require('string');
+// Debugging dirtyness
+var log = function (args, depth) { console.log(require('util').inspect(args, { colors: true, depth: depth })); };
 
 module.exports = function (grunt) {
-  var log = function (args, depth) { console.log(require('util').inspect(args, { colors: true, depth: depth })); };
-
-
-  grunt.task.registerTask('dump', 'Dump a database using pg_dump in your system', function () {
+  grunt.registerTask('dump-execute', 'Dump a database using pg_dump in your system', function () {
     var done = this.async();
-
-    // Check installation of pg_dump in the system
-    cp.exec('pg_dump', function (err, stdout, stderr) {
-      if (err) {
-        if (err.code === 127) {
-          grunt.fail.fatal('Using the dump Task requires pg_dump installed on your system, please check https://github.com/adunstan/postgresql-dev/tree/master/src/bin/pg_dump');
-        }
-      }
-    });
-
+    
   });
+  grunt.registerTask('dump', ['check-pg_dump', 'dump-execute']);
 
 };
